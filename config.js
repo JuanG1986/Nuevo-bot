de0154b8598e19a5bf3c2a6a30b1e7801f28b18c
JuanG1986 <126287790+JuanG1986@users.noreply.github.com>
2024-09-29 20:25:45 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,114 @@
+import {watchFile, unwatchFile} from 'fs';
+import chalk from 'chalk';
+import {fileURLToPath} from 'url';
+import fs from 'fs'; 
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import moment from 'moment-timezone';
+
+global.botnumber = ""
+global.confirmCode = ""
+
+global.owner = [
+  ['inserte numero del owner', '👑   Creador 👑', true],
+  ['', '  Collaborator 1 💫', true],
+  ['', '  - Collaborator 2 ', true],
+  ['', '  - Collaborator 3 ', true],
+  ['', ' - Collaborator 4 ', true],
+  ['', '  - Collaborator 5 ', true],
+  ['', '  - Collaborator 6 ', true],
+  ['', '  - Collaborator 7 ', false],
+  ['', '  - Collaborator 8 ', true],
+  ['','  - Tester ', true],
+  [''],
+  [''],
+  [''],
+  [''],
+  [''],
+  [''],
+  [''],
+  [''], 
+  [''],  
+  [''],
+  [''],
+  ['']
+];
+
+global.suittag = [''];
+global.prems = [''];
+
+global.packname = '𝐀𝐋𝐀𝐒𝐓𝐎𝐑-𝐁𝐎𝐓';
+global.author = 'Base hecha por astral';
+global.wm = 'wm';
+global.titulowm = 'titulowm';
+global.titulowm2 = `titulowm2`
+global.igfg = 'igfg';
+global.wait = '*Cargando...*';
+
+global.imagen1 = fs.readFileSync('./imagen1');
+global.imagen2 = fs.readFileSync('./imagen2');
+global.imagen3 = fs.readFileSync('./imagen3');
+global.imagen4 = fs.readFileSync('./imagen4');
+global.imagen5 = fs.readFileSync('./imagen5');
+global.imagen6 = fs.readFileSync('./imagen6');
+global.imagen7 = fs.readFileSync('./imagen7');
+global.imagen8 = fs.readFileSync('./imagen8');
+global.imagen9 = fs.readFileSync('./imagen9');
+
+global.mods = [];
+
+//* *******Tiempo***************
+global.d = new Date(new Date + 3600000);
+global.locale = 'es';
+global.dia = d.toLocaleDateString(locale, {weekday: 'long'});
+global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'});
+global.mes = d.toLocaleDateString('es', {month: 'long'});
+global.año = d.toLocaleDateString('es', {year: 'numeric'});
+global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
+//* ****************************
+global.wm2 = `${dia} ${fecha}\n`;
+global.gt = '';
+global.mysticbot = '';
+global.md = '';
+global.mysticbot = '';
+global.waitt = '*Cargando...*';
+global.waittt = '*Cargando...*';
+global.waitttt = '*Cargando...*';
+global.nomorown = '5219993404349';
+global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
+global.cmenut = '❖––––––『';
+global.cmenub = '┊✦ ';
+global.cmenuf = '╰━═┅═━––––––๑\n';
+global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     ';
+global.dmenut = '*❖─┅──┅〈*';
+global.dmenub = '*┊»*';
+global.dmenub2 = '*┊*';
+global.dmenuf = '*╰┅────────┅✦*';
+global.htjava = '⫹⫺';
+global.htki = '*⭑•̩̩͙⊱•••• ☪*';
+global.htka = '*☪ ••••̩̩͙⊰•⭑*';
+global.comienzo = '• • ◕◕════';
+global.fin = '════◕◕ • •';
+global.botdate = `*[ 📅 ] Fecha:*  ${moment.tz('America/Mexico_City').format('DD/MM/YY')}`;
+global.bottime = `*[ ⏳ ] Hora:* ${moment.tz('America/Mexico_City').format('HH:mm:ss')}`;
+global.fgif = {key: {participant: '0@s.whatsapp.net'}, message: {'videoMessage': {'title': wm, 'h': `Hmm`, 'seconds': '999999999', 'gifPlayback': 'true', 'caption': bottime, 'jpegThumbnail': fs.readFileSync('./Menu.png')}}};
+global.multiplier = 99;
+global.flaaa = [
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=',
+];
+//* ************************
+
+const file = fileURLToPath(import.meta.url);
+watchFile(file, () => {
+  unwatchFile(file);
+  console.log(chalk.redBright('Update \'config.js\''));
+  import(`${file}?update=${Date.now()}`);
+});
+
+global.apikasu = "https://apikasu.onrender.com"
+global.apikeykasu = "SebastianDevelop"
